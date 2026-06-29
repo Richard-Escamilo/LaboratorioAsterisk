613dbbb3a227db0ff8ad34020f123db59bad3b6c
Richard-Escamilo <eskmiilo@gmail.com>
2026-06-29 04:21:36 +0000

    Seguridad SonarCloud: tts.js usa nombres de archivo temporal unicos (no predecibles) en /tmp y los limpia despues de usarlos

--- call-monitor-backend/tts.js
+++ call-monitor-backend/tts.js
@@ -1,9 +1,10 @@
 const { execFile } = require("child_process");
 const path = require("path");
+const fs = require("fs");
+const crypto = require("crypto");
 
 const CONFIG_DIR = process.env.ASTERISK_CONFIG_DIR || "/asterisk-config";
 const SOUNDS_DIR = path.join(CONFIG_DIR, "sounds");
-const TEMP_WAV = "/tmp/parqueo_raw.wav";
 const FINAL_WAV = path.join(SOUNDS_DIR, "parqueo_mensaje.wav");
 
 function sanitizeText(text) {
@@ -14,10 +15,17 @@ function sanitizeText(text) {
 
 function generateParkingMessage(rawText) {
   const text = sanitizeText(rawText);
+  const uniqueId = crypto.randomBytes(8).toString("hex");
+  const tempWav = path.join("/tmp", `parqueo_raw_${uniqueId}.wav`);
+
   return new Promise((resolve, reject) => {
-    execFile("espeak-ng", ["-v", "es", "-w", TEMP_WAV, text], (err) => {
-      if (err) return reject(new Error("Error generando audio: " + err.message));
-      execFile("sox", [TEMP_WAV, "-r", "8000", "-c", "1", "-b", "16", FINAL_WAV], (err2) => {
+    execFile("espeak-ng", ["-v", "es", "-w", tempWav, text], (err) => {
+      if (err) {
+        fs.unlink(tempWav, () => {});
+        return reject(new Error("Error generando audio: " + err.message));
+      }
+      execFile("sox", [tempWav, "-r", "8000", "-c", "1", "-b", "16", FINAL_WAV], (err2) => {
+        fs.unlink(tempWav, () => {});
         if (err2) return reject(new Error("Error convirtiendo audio: " + err2.message));
         resolve({ path: FINAL_WAV });
       });
